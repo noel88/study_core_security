e2d538ea0985f8b9aece89bf7a6f38c1fd7be4bc
Xiah <xiah0526@gmail.com>
2024-01-31 10:40:50 +0900

    3 - 4) DB 연동 인증 처리(1) : CustomUserDetailsService

--- core-spring-security/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ core-spring-security/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.security.corespringsecurity.security.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +9,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
 
     @Bean
     protected PasswordEncoder passwordEncoder() {
