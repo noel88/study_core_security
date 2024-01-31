a891c0265b8517e0a45375f0aba249481607ae51
Xiah <xiah0526@gmail.com>
2024-01-31 11:40:35 +0900

    3 - 7) 로그아웃 및 인증에 따른 화면 보안 처리

--- core-spring-security/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ core-spring-security/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -54,7 +54,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/config").hasRole("ADMIN")
                 .anyRequest().authenticated()
             .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login_proc")
+                .defaultSuccessUrl("/")
+                .permitAll();
 
     }
 }
